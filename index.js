bbfd3566653716c0c0a19d1c890c3cacec937ad9
Thinker <sky456757@gmail.com>
2021-08-06 13:27:50 +0800

    Update index.js
    test

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ module.exports = Application({
     // do what ever you want after save comment
   },
 });
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1811314527:AAH4H6SAwceSqza3JEwJaZ4yKWJjXdvQWQ0';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+
+var Hi = "hi";
+if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+bot.sendMessage(msg.chat.id,"Hello dear user");
+}
+
+});
+
+
+
+
